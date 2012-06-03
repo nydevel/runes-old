0b5f53f602bf0ddd68ea8b3b331d001651b4d2b0
ai_boy@live.ru <ai_boy@live.ru>
2012-06-04 04:35:10 +0600

    added simple compile error support

--- script/runes.js
+++ script/runes.js
@@ -1,17 +1,28 @@
-﻿(function() {
+﻿(function () {
+
+    var errorCallback = undefined;
 
     runes = {
         run: function(source) {
-            if (typeof source === "string") {
-                //preprocess code
-                var lexList = runes.lexer.analize(source);
-                var languageNodes = runes.parser.analize(lexList);
-                //code is valid. now we can run it
-                runes.vm.run(languageNodes);
-            } else {
-                throw new Error("for the moment only compilation from string is supported");
+            try {
+                if (typeof source === "string") {
+                    //preprocess code
+                    var lexList = runes.lexer.analize(source);
+                    var languageNodes = runes.parser.analize(lexList);
+                    //code is valid. now we can run it
+                    runes.vm.run(languageNodes);
+                } else {
+                    throw new Error("for the moment only compilation from string is supported");
+                }
+            } catch (e) {
+                if (this.onError) {
+                    this.onError(e);
+                } else {
+                    throw e;
+                }
             }
-        }
+        },
+        onError: undefined
     }
 
 })();
